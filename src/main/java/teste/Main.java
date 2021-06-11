1b44e336c4fed585c21e6abb69d65c32ffeb0e70
Rafael <76920313+rafaelassumpcao274@users.noreply.github.com>
2021-06-11 20:27:50 -0300

    update
    modificação do codigo principal

--- src/main/java/teste/Main.java
+++ src/main/java/teste/Main.java
@@ -8,12 +8,10 @@ public class Main {
 		Data mapa = new Data();
 		int x=3;
 		int y=6;
-
 		int pos_ant_x=0;
 		int pos_ant_y=0;
-		int caminhos = 0;
+		boolean caminhos_l = true;
 		int passos=0;
-		boolean teste = false;
 		Scanner scan = new Scanner(System.in);
 		if(x>3 && y>8) 
 		{
@@ -25,150 +23,99 @@ public class Main {
 			System.exit(0);
 		}
 		do{
-			System.out.println(" ");	
-			if(mapa.map[x][y] == 1)
-			{
-				x=pos_ant_x;
-				y= pos_ant_y;
-				if(teste) {
-					x=pos_ant_x;
-					y= pos_ant_y;
-					
-					break;
-				}else {
-					if(((x<3)||(y<9))&&((x>0)&&(y>0))){
-						caminhos=0;
-						if(mapa.map[x-1][y] != 0) {
-							if(mapa.map[x][y-1] !=0) {
-								if(x != 3) {
-									if(mapa.map[x+1][y] != 0) {
-										x=pos_ant_x;
-										y= pos_ant_y;
-									}else {
-										x++;
-									}									
-								}else{
-									if(mapa.map[x][y+1] != 0) {
-										x=pos_ant_x;
-										y= pos_ant_y;
-									}else {
-										caminhos++;
-										y++;
-									}	
-								}
-								
-							}else {
-								y--;
-							}
-						}else {
+
+			System.out.println(" ");
+			if((x>1) && (y>1)) {
+
+				if(caminhos_l) {
+					if(x>y) {
+						if(mapa.map[x-1][y] != 1) {
 							x--;
+						}else {
+							caminhos_l=false;
+						}
+
+					}else{
+						if(mapa.map[x][y-1] != 1) {
+							y--;
+						}else {
+							caminhos_l = false;
+						}
+					};	
+				}else {
+					
+					if(mapa.map[x-1][y] != 1) {							
+						if(mapa.map[x-2][y] != 1) {
+							caminhos_l=true;
+						}
+						x--;
+					}else if(mapa.map[x][y-1] !=1) {
+						if(mapa.map[x][y-2] != 1 ) {
+							caminhos_l=true;
+						}
+						y--;
+					}else if((x<3)&&(y<8)) {
+						if(mapa.map[x][y+1] !=1) {
+							if(mapa.map[x][y+2] != 1) {
+								caminhos_l=true;
+							}
+						}else if(mapa.map[x+1][y] != 1) {
+							if(mapa.map[x+2][y] != 1) {
+								caminhos_l=true;
+							}
+							x++;
 						}
 					}else {
-						if(x==3) {
-							if(y<1){
-								if(mapa.map[x-1][y] !=0) {
-									if(mapa.map[x][y+1] !=0) {
-										System.out.println("Erro robo preso");
-										break;
-									}else {
-										y++;
-									}
-								}else {
-									x--;
-								}								
-							}else if(y>8){
-								if(mapa.map[x-1][y] !=0) {
-									if(mapa.map[x][y-1] !=0) {
-										System.out.println("Erro robo preso");
-										break;
-									}else {
-										y--;
-									}
-								}else {
-									x--;
-									}	
-								
-							}
-						}else if(x<1){
-							if(mapa.map[x+1][y] !=0) {
-								if(mapa.map[x][y-1] !=0) {
-									if(y<9) {
-										if(mapa.map[x][y+1] == 0) {
-											y++;
-											caminhos++;
-										}									
-										}
-								}else {
-									y--;
-								}
-							}else {
-								x--;
-							}	
-						}						
-					}
-					teste=true;
+						if(x>y) {
+							x++;
+						}else {
+							y++;
+						}
+						}
+
+
+					
 				}
-				continue;
-				
-			}else {
-				teste = false;
-				pos_ant_x = x;
-				pos_ant_y = y;
 
-			    for (int i = 0; i < mapa.map.length; ++i) {
-			        for(int j = 0; j < mapa.map[i].length; ++j) {
-			        	if(i==x && j ==y) {
-			        		System.out.print("*");
-			        	}else {
-			        		System.out.print(mapa.map[i][j]);
-			        	}		          
-			        	if(j>= 9)
-			        		System.out.println(",");
-			        }
-			      }
-			    if(y>2) {
-			    	if(x>2) {
-			    		if((mapa.map[x][y-2]== 0)&&(mapa.map[x][y-1]== 0)) {
-			    			caminhos = 0;
-			    		};
-			    		if((mapa.map[x-2][y]== 0)&&(mapa.map[x-1][y]== 0)) {
-			    			caminhos = 0;
-			    		};
-			    	}
-	
-			    }
-
-			    if(caminhos>1) {
-			    	if(x<y) {
-			    		x++;
-			    	}else {
-			    		y++;
-			    	}
-			    
-			    
-			    if(x>=3) {
-			    	caminhos=0;
-			    }
-			    }else {
-				    if(x>y) {
-				    	x--;
-				    }else{
-				    	y--;
-				    };
-				   
-			    }
-
+			}else if((x>0)||(y>0))
+			{
+				if((y>0) && (x<1)) {
+					if(mapa.map[x][y-1] != 1) {
+						y--;
+					}else if(mapa.map[x+1][y] != 1) {
+						x++;
+					}
+				}else {
+					if(mapa.map[x-1][y] != 1) {
+						x--;
+					}else if(mapa.map[x][y+1] !=1){
+						y++;
+					}
+				}
 			}
+			
+		
 
-		    
-//		    System.out.println("Valor x:");
-//		    x=scan.nextInt();
-//		    System.out.println("Valor y:");
-//		    y=scan.nextInt();
+		    for (int i = 0; i < mapa.map.length; ++i) {
+		        for(int j = 0; j < mapa.map[i].length; ++j) {
+		        	if(i==x && j ==y) {
+		        		System.out.print("*");
+		        	}else {
+		        		System.out.print(mapa.map[i][j]);
+		        	}		          
+		        	if(j>= 9)
+		        		System.out.println(",");
+		        }
+		      }		
+
+
+
 
 		    passos++;
-
-		}while((x>=0) && (y>=0));
+		    if((x<1)&&(y<1)) {
+		    	break;
+		    }
+		}while(true);
 
 		System.out.println("Foi dado "+passos+" passos para chegar a terra!!");
 	}
